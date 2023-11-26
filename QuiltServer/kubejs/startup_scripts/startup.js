2d2ee02ba2822142e79a917c31f87fa8df2ce4a6
Amy Patterson <amyfromearth@gmail.com>
2023-11-25 18:30:15 -0700

    KubeJS item stacks and fuel values

--- QuiltServer/kubejs/startup_scripts/startup.js
+++ QuiltServer/kubejs/startup_scripts/startup.js
@@ -5,8 +5,12 @@
 console.info('KubeJS Startup complete (Loaded startup scripts)')
 
 ItemEvents.modification(event => {
-    //Modify any item here
+    //Potions stack
     event.modify("minecraft:potion", item => {item.setMaxStackSize(16)})
     event.modify("minecraft:splash_potion", item => {item.setMaxStackSize(16)})
     event.modify("minecraft:lingering_potion", item => {item.setMaxStackSize(16)})
+    //Suspicious stew stacks
+    event.modify("minecraft:suspicious_stew", item => {item.setMaxStackSize(16)})
+    //Arrows burn
+    event.modify("minecraft:arrow", item => {item.setBurnTime(100)})
 })
